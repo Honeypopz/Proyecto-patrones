842b83e262c72c2774a59fd6acf90b5b23d3c5db
Jason <Jason@Jason>
2026-03-09 21:51:40 -0600

    Base para el proyecto

---
+++ src/main/java/proyecto/Caso01Application.java
@@ -0,0 +1,13 @@
+package proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Caso01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Caso01Application.class, args);
+	}
+
+}
